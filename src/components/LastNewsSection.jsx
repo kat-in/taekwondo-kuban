9213d728bd67768782efa93402ceec423107d4f9
kat-in <newkatelife@gmail.com>
2026-06-03 21:35:22 +0300

    feat: вывод новостей в NewsPage и в LastNews на главной.

--- src/components/LastNewsSection.jsx
+++ src/components/LastNewsSection.jsx
@@ -1,9 +1,23 @@
 import PageSection from "./PageSection"
+import newsData from "../data/newsData"
 
 const LastNewsSection = () => {
+    const sortedNews = [...newsData].sort((a, b) => b.date.localeCompare(a.date))
+    const lastNews = sortedNews.slice(0, 4).map((item) => {
+        
+        return (
+            <article className="news_item" id={item.id} key={item.id}>
+                <div className="news_date">{item.displayDate}</div>
+                <div className="news_content">
+                    <div>{item.content}</div>
+                </div>
+            </article>
+        )
+    })
+
     return (
         <PageSection title='Последние новости' theme='light'>
-        <div>Тут будут последние новости</div>
+        <div>{lastNews}</div>
         </PageSection>
     )
 }
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,6 +1,37 @@
+import newsData from "../data/newsData"
+
 const NewsPage = () => {
-    return ( 
-    <h1>Последние новости</h1>
+    const sortedNews = [...newsData].sort((a, b) => b.date.localeCompare(a.date))
+    const news = sortedNews.map((item) => {
+        const image = item.image && <img className='news_card_img' src={item.image.url}/>
+        const images = item.images &&  item.images.map((image) => <img className='news_card_images' src={image.url} key={image.url}/>)
+        const details = item.details && <div className="news_card_details">{item.details}</div>
+        return (
+            <article className="news_card" id={item.id} key={item.id}>
+                <div className="news_date">{item.displayDate}</div>
+                <div className="news_content">
+                    <div>{item.content}</div>
+                    {image}
+                    {details}
+                    {images}
+                </div>
+            </article>
+        )
+    })
+
+    return (
+        <>
+            <h1>Новости</h1>
+             <ul className="news_filter">
+                <li>Аттестация</li>
+                <li>Соревнования</li>
+                <li>Все новости</li>
+            </ul>
+            <div className="news__section">
+                {news}
+
+            </div>
+        </>
     )
 }
 
