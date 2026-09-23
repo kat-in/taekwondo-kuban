dd5bbd00ba7b045e27409088ace64e5cdfa96df4
kat-in <newkatelife@gmail.com>
2026-09-23 13:18:19 +0300

    fix: исправить пути к шрифтам и настроить открытие страницы при запуске

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true,      
     proxy: {
       '/api': 'http://localhost:5001',
       '/uploads': 'http://localhost:5001'
