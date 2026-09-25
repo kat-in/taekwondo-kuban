8ed7de44b04ed12bb8cecab2ccec2f841cb7d822
kat-in <newkatelife@gmail.com>
2026-09-25 23:14:06 +0300

    feat: добавить название ассоциации в хедер
    - текст в три строки рядом с логотипами, скрыт на главной
    - плавное уменьшение логотипов и текста на узких экранах
    - запретить переносы в горизонтальном меню

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
+import { useLocation } from "react-router-dom"
 import Navigation from "./NavBar/Navigation"
 import Burger from "./NavBar/Burger"
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const location = useLocation()
+    const isHome = location.pathname === '/'
+
     return (
         <div className='header' >
             <div className='header__container'>
             <Link to='/'> <div className='header__logo'>
                 <div className='logo1'></div>
                 <div className='logo2'></div>
+                {!isHome && <div className='header__logo-text'><span className='header__logo-line'>Краснодарская городская</span><span className='header__logo-line'>Ассоциация Тхэквондо</span><span className='header__logo-line'>МУ ДУК КВАН</span></div>}
             </div>
             </Link>
             <Navigation />
